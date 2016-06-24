fc0cce859576cef8b3975be5eec97c16f6ac5efb
jkbits1 <jkbits1@users.noreply.github.com>
2016-06-24 23:29:52 +0100

    data sent and received to js module, refactored code

--- d3basic.elm
+++ d3basic.elm
@@ -1,9 +1,12 @@
 port module TestInterop exposing (..)
 
+import Wheel exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Events exposing (..)
 
+import List exposing (..)
 import String
 
 main = Html.program
@@ -13,31 +16,6 @@ main = Html.program
   , update = update
   , subscriptions = subscriptions
   }
-
--- tuple or record?
-type alias WheelItem =
-  {
-    name : String
-  , value : Int
-  }
-
-type alias WheelData = List WheelItem
-
-wheelData : WheelData
-wheelData = [
-  {name = "One",    value = 15},
-  {name = "Two",    value = 9},
-  {name = "Three",  value = 9},
-  {name = "Four",   value = 19},
-  {name = "Five",   value = 12},
-  {name = "Six",    value = 14},
-  {name = "Seven",  value = 21},
-  {name = "Eight",  value = 18},
-  {name = "Nine",   value = 8}
-  ]
-
-wheelItemNames : List String
-wheelItemNames = List.map .name wheelData
 
 type alias Model =   
   {
@@ -69,11 +47,14 @@ update msg model =
 
     Check ->
 --      (model, check model.wheelItems)
-      (model, check wheelItemNames)
+      (model, check <| wheelItemNames model.wheelItems)
 
     Suggest newDataItems ->
---      (Model model.wheelItems newDataItems, Cmd.none)
-      (initialModel, Cmd.none)
+      (Model (createNewWheelStrings <| createFullListFromResponses newDataItems)
+        []
+        , Cmd.none)
+--    (Model model.wheelItems newDataItems, Cmd.none)
+
 
 -- subscriptions
 
@@ -87,6 +68,9 @@ view model = div []
   [ 
     input [ onInput Change ] []
   , button [ onClick Check ] [ text "check"]
-  , div [] [ text <| String.join ", " model.dataProcessedItems ]
+  , div [] [ text <| String.join ", "
+--      model.dataProcessedItems
+        <| map .name model.wheelItems
+      ]
   ]
   
