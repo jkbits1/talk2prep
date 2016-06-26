13fa36922c83bb8dd6e40c85c7a7a6f0a4d7d646
jkbits1 <jkbits1@users.noreply.github.com>
2016-06-26 01:10:52 +0100

    add sz info to array in js iop file

--- Wheel.elm
+++ Wheel.elm
@@ -6,11 +6,51 @@ import List exposing (..)
 type alias WheelItem =
   {
     name : String
-  , value : Int
-  , sz : Int
+--  , value : Int
+--  , sz : Int
   }
 
 type alias WheelData = List WheelItem
+
+wheelItemNames : List WheelItem -> List String
+wheelItemNames ws = map .name ws
+
+nth idx xs = Maybe.withDefault "" <| head <| drop idx xs
+nthData idx xs =
+  Maybe.withDefault
+--    { name = "Test", value = 1, sz = 1 }
+    { name = "Test" }
+    <| head <| drop idx xs
+
+headWD : List WheelData -> WheelData
+headWD wds =
+  Maybe.withDefault
+--    [{ name = "Test", value = 1, sz = 1 }]
+    [{ name = "Test" }]
+    <| head wds
+
+--responses = ["X"]
+
+getResponseOrDefault : List String -> WheelData -> Int ->  String
+getResponseOrDefault resp data idx =
+  if idx < length resp then
+    nth idx resp
+  else
+    .name <| nthData idx data
+
+createFullListFromResponses : WheelData -> List String -> List String
+createFullListFromResponses ws rs =
+  List.map
+    (getResponseOrDefault rs ws)
+    [0..(List.length ws)]
+
+createNewWheelStrings : WheelData -> List String -> WheelData
+createNewWheelStrings ws ns =
+  map2 (\a b ->
+--    { name = a, value = .value b, sz = .sz b })
+    { name = a}
+       )
+    ns ws
 
 wheelData1 : WheelData
 wheelData1 = [
@@ -32,15 +72,24 @@ wheelData1 = [
 --  {name = "Seven",  value = 21, sz = 8},
 --  {name = "Eight",  value = 18, sz = 8},
 --  {name = "Nine",   value = 8, sz = 8}
-  {name = "1",    value = 15, sz = 8},
-  {name = "2",    value = 9, sz = 8},
-  {name = "3",  value = 9, sz = 8},
-  {name = "4",   value = 19, sz = 8},
-  {name = "5",   value = 12, sz = 8},
-  {name = "6",    value = 14, sz = 8},
-  {name = "7",  value = 21, sz = 8},
-  {name = "8",  value = 18, sz = 8},
-  {name = "9",   value = 8, sz = 8}
+--  {name = "1",    value = 15, sz = 8},
+--  {name = "2",    value = 9, sz = 8},
+--  {name = "3",  value = 9, sz = 8},
+--  {name = "4",   value = 19, sz = 8},
+--  {name = "5",   value = 12, sz = 8},
+--  {name = "6",    value = 14, sz = 8},
+--  {name = "7",  value = 21, sz = 8},
+--  {name = "8",  value = 18, sz = 8},
+--  {name = "9",   value = 8, sz = 8}
+  {name = "1"},
+  {name = "2"},
+  {name = "3"},
+  {name = "4"},
+  {name = "5"},
+  {name = "6"},
+  {name = "7"},
+  {name = "8"},
+  {name = "9"}
   ]
 
 wheelData2 : WheelData
@@ -54,65 +103,27 @@ wheelData2 = [
 --  {name = "SevenX",  value = 21, sz = 6},
 --  {name = "EightX",  value = 18, sz = 6},
 --  {name = "NineX",   value = 8, sz = 6}
-  {name = "1x",    value = 15, sz = 6},
-  {name = "2x",    value = 9, sz = 6},
-  {name = "3x",  value = 9, sz = 6},
-  {name = "4x",   value = 19, sz = 6},
-  {name = "5x",   value = 12, sz = 6},
-  {name = "6x",    value = 14, sz = 6},
-  {name = "7x",  value = 21, sz = 6},
-  {name = "8x",  value = 18, sz = 6},
-  {name = "9x",   value = 8, sz = 6}
+  {name = "1x"},
+  {name = "2x"},
+  {name = "3x"},
+  {name = "4x"},
+  {name = "5x"},
+  {name = "6x"},
+  {name = "7x"},
+  {name = "8x"},
+  {name = "9x"}
   ]
 
 wheelData3 : WheelData
 wheelData3 = [
---  {name = "OneX",    value = 15, sz = 6},
---  {name = "TwoX",    value = 9, sz = 6},
---  {name = "ThreeX",  value = 9, sz = 6},
---  {name = "FourX",   value = 19, sz = 6},
---  {name = "FiveX",   value = 12, sz = 6},
---  {name = "SixX",    value = 14, sz = 6},
---  {name = "SevenX",  value = 21, sz = 6},
---  {name = "EightX",  value = 18, sz = 6},
---  {name = "NineX",   value = 8, sz = 6}
-  {name = "1-",    value = 15, sz = 4},
-  {name = "2-",    value = 9, sz = 4},
-  {name = "3-",  value = 9, sz = 4},
-  {name = "4-",   value = 19, sz = 4},
-  {name = "5-",   value = 12, sz = 4},
-  {name = "6-",    value = 14, sz = 4},
-  {name = "7-",  value = 21, sz = 4},
-  {name = "8-",  value = 18, sz = 4},
-  {name = "9-",   value = 8, sz = 4}
+  {name = "1-"},
+  {name = "2-"},
+  {name = "3-"},
+  {name = "4-"},
+  {name = "5-"},
+  {name = "6-"},
+  {name = "7-"},
+  {name = "8-"},
+  {name = "9-"}
   ]
 
-wheelItemNames : List WheelItem -> List String
-wheelItemNames ws = map .name ws
-
-nth idx xs = Maybe.withDefault "" <| head <| drop idx xs
-nthData idx xs =
-  Maybe.withDefault { name = "Test", value = 1, sz = 1 } <| head <| drop idx xs
-
-headWD : List WheelData -> WheelData
-headWD wds =
-  Maybe.withDefault [{ name = "Test", value = 1, sz = 1 }] <| head wds
-
---responses = ["X"]
-
-getResponseOrDefault : List String -> WheelData -> Int ->  String
-getResponseOrDefault resp data idx =
-  if idx < length resp then
-    nth idx resp
-  else
-    .name <| nthData idx data
-
-createFullListFromResponses : WheelData -> List String -> List String
-createFullListFromResponses ws rs =
-  List.map
-    (getResponseOrDefault rs ws)
-    [0..(List.length ws)]
-
-createNewWheelStrings : WheelData -> List String -> WheelData
-createNewWheelStrings ws ns =
-  map2 (\a b -> { name = a, value = .value b, sz = .sz b }) ns ws
--- d3basic-iop.js
+++ d3basic-iop.js
@@ -9,7 +9,21 @@ var app = Elm.TestInterop.embed(node);
 
 app.ports.check.subscribe( data => {
 
-  showCircle(data);
+  var dataSz = [];
+
+  dataSz[0] = data[0].map(val => {
+    return {name: val.name, value: 9, sz: 8}
+  });
+
+  dataSz[1] = data[1].map(val => {
+    return {name: val.name, value: 9, sz: 6}
+  });
+
+  dataSz[2] = data[2].map(val => {
+    return {name: val.name, value: 9, sz: 4}
+  });
+
+  showCircle(dataSz);
 
   var names = data[0].map(val => val.name);
 
